a119c7e4cef941d87d307b97b33c031236ad806e
mbonikev <mbonimpayekevin@gmail.com>
2025-11-23 16:52:59 +0200

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainColor: "var(--main_color)",
+        mainWeak: "var(--main_color_weak)",
+        cardBg: "var(--card_bg)",
+        bodyBg: "var(--body_bg)",
+        textColor: "var(--text_color)",
+        textColorWeak: "var(--text_color_weak)",
+        linesColor: "var(--lines_color)",
+      },
+    },
   },
   plugins: [],
-}
+};
