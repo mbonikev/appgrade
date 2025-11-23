07e9f0c4165289399ea27ada7296467debe5b42a
mbonikev <mbonimpayekevin@gmail.com>
2025-11-23 15:09:09 +0200

    routes and tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import './App.css'
+import './styles/App.css'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
